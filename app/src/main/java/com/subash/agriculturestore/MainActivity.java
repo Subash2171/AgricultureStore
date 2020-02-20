9db0b7ec954cf5334695659812767eb3f7a906a2
subash2171 <subashgurung217@gmail.com>
2020-02-20 14:13:35 +0545

    Adding full screen activities

--- app/src/main/java/com/subash/agriculturestore/MainActivity.java
+++ app/src/main/java/com/subash/agriculturestore/MainActivity.java
@@ -35,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
