3eed1cb81f4a7f093d2f5e6c4ab9d96f2dd05c5c
subash2171 <subashgurung217@gmail.com>
2020-02-20 14:12:46 +0545

    importing libraries

--- app/src/main/java/com/subash/agriculturestore/MainActivity.java
+++ app/src/main/java/com/subash/agriculturestore/MainActivity.java
@@ -1,9 +1,27 @@
 package com.subash.agriculturestore;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     @Override
